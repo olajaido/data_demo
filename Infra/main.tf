c223fa2b2960a3f0ae65ea60fd54c96a7cf055c3
Olajide Adeluwoye <devops@eclatit.co.uk>
2025-01-11 01:46:52 +0000

    fix code structure

--- Infra/main.tf
+++ Infra/main.tf
@@ -235,11 +235,11 @@ resource "aws_security_group" "ecs_tasks" {
 resource "aws_ecs_task_definition" "dashboard" {
   family                   = "retail-dashboard"
   requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = "256"
-  memory                  = "512"
-  execution_role_arn      = aws_iam_role.ecs_execution_role.arn
-  task_role_arn           = aws_iam_role.ecs_task_role.arn
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
